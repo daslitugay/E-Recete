ec0a135e67098b09906da1ffa600845d167cb6ce
Derya Aslı Tugay <149428526+daslitugay@users.noreply.github.com>
2026-04-04 17:27:27 +0300

    Add server and Vercel configuration files for backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const app=require('./app');
+const dotenv=require('dotenv');
+const mongoose=require('mongoose');
+
+dotenv.config();
+
+let isConnected=false;
+
+module.exports=async (req, res) => {
+    if (!isConnected) {
+        try {
+            await mongoose.connect(process.env.MONGODB_URI);
+        } catch (error) {
+            console.error('Error connecting to MongoDB:', error);
+        }
+        isConnected = true;
+    }
+
+    return app(req, res);
+}
